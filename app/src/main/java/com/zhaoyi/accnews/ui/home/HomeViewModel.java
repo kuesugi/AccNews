bacd632e62202696468b284b5ab8e2e1fd051680
zhaoyi <jackfield1611@gmail.com>
2020-08-01 03:05:27 -0400

    add repository and viewmodel

---
+++ app/src/main/java/com/zhaoyi/accnews/ui/home/HomeViewModel.java
@@ -0,0 +1,27 @@
+package com.zhaoyi.accnews.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.zhaoyi.accnews.model.NewsResponse;
+import com.zhaoyi.accnews.repository.NewsRepository;
+
+public class HomeViewModel extends ViewModel {
+
+    private final NewsRepository repository;
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+
+    public HomeViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    public LiveData<NewsResponse> getTopHeadlines() {
+        return Transformations.switchMap(countryInput, repository::getTopHeadlines);
+    }
+}
