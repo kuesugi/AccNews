fcfd85c6dce5eee2098e55f8545bef252c9dd7bc
zhaoyi <jackfield1611@gmail.com>
2020-08-03 18:02:53 -0400

    update view binding

--- app/src/main/java/com/zhaoyi/accnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/zhaoyi/accnews/ui/search/SearchFragment.java
@@ -8,40 +8,60 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.zhaoyi.accnews.R;
+import com.zhaoyi.accnews.databinding.FragmentSearchBinding;
 import com.zhaoyi.accnews.repository.NewsRepository;
 import com.zhaoyi.accnews.repository.NewsViewModelFactory;
 
 public class SearchFragment extends Fragment {
-    private SearchViewModel viewModel;
+	private SearchViewModel viewModel;
+	private FragmentSearchBinding binding;
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
+	public SearchFragment() {
+		// Required empty public constructor
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		binding = FragmentSearchBinding.inflate(inflater, container, false);
+		return binding.getRoot();
+	}
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
 
-        NewsRepository repository = new NewsRepository(getContext());
-        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
-                .get(SearchViewModel.class);
-        viewModel.setSearchInput("android");
-        viewModel.searchNews().observe(
-            getViewLifecycleOwner(), newsResponse -> {
-                if (newsResponse != null) {
-                    Log.d("SearchFragment", newsResponse.toString());
-                }
-        });
-    }
+		binding.newsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				if (!query.isEmpty()) {
+					viewModel.setSearchInput(query);
+				}
+				binding.newsSearchView.clearFocus();
+				return true;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				return false;
+			}
+		});
+
+		NewsRepository repository = new NewsRepository(getContext());
+		viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+				.get(SearchViewModel.class);
+		// viewModel.setSearchInput("android");
+		viewModel.searchNews().observe(
+			getViewLifecycleOwner(), newsResponse -> {
+				if (newsResponse != null) {
+					Log.d("SearchFragment", newsResponse.toString());
+				}
+		});
+	}
 }
